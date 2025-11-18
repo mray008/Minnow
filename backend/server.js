a09fb2dd59e566dbd2f778381a81b9a81d74f2d7
Stanley Synenko <ssyne001@odu.edu>
2025-11-18 00:55:25 -0500

    this is gonna be a really fun merge. i forgot to pull my branch forward hehehehehe. started (re)working the backend, and hooked it up to a database.

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,84 @@ app.use(cors()); // allow requests from frontend
 app.use(express.json());
 
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  const user = users.find(u => u.username === username && u.password === password);
+  connection.execute(
+  "select * from users where username=? and password=?",
+    [
+      req.body.Username,
+      req.body.Password,
+    ],
+    function (err, result) {
+      if (err) {
+        console.log("Error");
+        res.send(err);
+      } else {
+        if (result.length == 1) {
+          // console.log("Result");
 
-  if (!user) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
+          if (req.body.Password == result[0].password) {
+            res.json({
+              status: 200,
+              message: "User logged in successfully!",
+              data: result,
+            });
+          }
+          else {
+            res.status(403).json({
+              status: 403,
+              message: "Password is not correct",
+            });
+          }
+        }
+        else {
+          res.status(403).json({
+            status: 403,
+            message: "Password is not correct",
+          });
+        }
+      }
+    }
+  );
+});
 
-  res.json({ username: user.username, role: user.role });
+app.post("/register", (req, res) => {
+  connection.execute(
+    "select * from user_information where username=?",
+    [
+      req.body.Username,
+    ],
+    function(err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (result.length == 0) {
+          connection.execute(
+            "insert into user_information (username,password,role) values (?,?,?)",
+            [
+              req.body.Username,
+              req.body.Password,
+              "student",
+            ],
+            function (err, result) {
+              if (err) {
+                res.send(err);
+              } else {
+                res.json({
+                  status: 200,
+                  message: "User created successfully",
+                  data: result,
+                });
+              }
+            }
+          );
+        } else {
+          res.status(403).json({
+            status: 403,
+            message: "Account already exists!",
+          })
+        }
+      }
+    }
+  );
 });
 
 const PORT = process.env.PORT || 5000;
