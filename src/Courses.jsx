db7a15eb4567be7c7c951fd4b21a60512fa6f458
ssyne001 <156539476+ssyne001@users.noreply.github.com>
2025-12-01 13:29:20 -0500

    Update Courses.jsx

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Courses.css";
 import mathImage from "./assets/math.jpg"; 
 import englishImage from "./assets/english.png";
